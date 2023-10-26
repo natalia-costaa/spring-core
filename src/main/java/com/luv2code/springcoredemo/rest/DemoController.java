68fa2052872aa9f70d1a185fea37cb4c83caadce
natalia-costaa <ncalbuq@gmail.com>
2023-10-26 11:07:15 -0300

    Update DemoController.java
    Testando o @Qualifier no construtor

--- src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
+++ src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -2,6 +2,7 @@ package com.luv2code.springcoredemo.rest;
 
 import com.luv2code.springcoredemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +12,7 @@ public class DemoController {
     private Coach myCoach;
 
     @Autowired
-    public DemoController(Coach myCoach) {
+    public DemoController(@Qualifier("baseballCoach") Coach myCoach) {
         this.myCoach = myCoach;
     }
 
