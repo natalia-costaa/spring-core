8195a964df719e04422c1fa7a4f6e50d54f58d6a
natalia-costaa <ncalbuq@gmail.com>
2023-10-26 11:06:38 -0300

    Update TrackCoach.java
    Criação de classe

--- src/main/java/com/luv2code/springcoredemo/common/TrackCoach.java
+++ src/main/java/com/luv2code/springcoredemo/common/TrackCoach.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class TrackCoach implements Coach{
-    
+
     @Override
     public String getDailyWorkout() {
         return "Run a hard 5k.";
