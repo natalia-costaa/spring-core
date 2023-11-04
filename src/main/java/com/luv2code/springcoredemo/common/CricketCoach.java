39cfb440096249cba243ae3792e203fa2985a647
natalia-costaa <ncalbuq@gmail.com>
2023-11-04 02:27:20 -0300

    Update CricketCoach.java
    uso do @PostConstructor  @PreDestroy para demonstrar os metodos de ciclo de vida do Bean

--- src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
+++ src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
@@ -1,13 +1,14 @@
 package com.luv2code.springcoredemo.common;
 
 import com.luv2code.springcoredemo.common.Coach;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class CricketCoach implements Coach {
 
 
@@ -18,5 +19,15 @@ public class CricketCoach implements Coach {
     @Override
     public String getDailyWorkout() {
         return "Practice fast bowling for 15 minutes";
+    }
+
+    @PostConstruct
+    public void doMyStartupStuff() {
+        System.out.println("In doMyStartupStuff(): " + getClass().getSimpleName());
+    }
+
+    @PreDestroy
+    public void doMyCleanupStuff() {
+        System.out.println("In doMyCleanupStuff(): " + getClass().getSimpleName());
     }
 }
